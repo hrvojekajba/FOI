4b07b331f482c39f158b5adaf0a8ecfe8b7e85ee
bluzmija <172497559+bluzmija@users.noreply.github.com>
2024-11-04 15:10:45 +0100

    Changed zad1.cpp in Polja

--- Prog1/Ekstra/VjezbePTG/Polja/zad1.cpp
+++ Prog1/Ekstra/VjezbePTG/Polja/zad1.cpp
@@ -2,6 +2,7 @@
 
 int main(){
     float polje[5]{};
+    float max = 0, min = 1000.0;
 
     for (int i = 0; i <= 4; i++){
         float unos;
@@ -12,7 +13,16 @@ int main(){
     }
     for (int i = 4; i >= 0; i--){
         std::cout << polje[i] << "\n";
+        if (polje[i] < min){
+            min = polje[i];
+        }
+        if (polje[i] > max){
+            max = polje[i];
+        }
     }
 
+    std::cout << "Max: " << max << "\n" << "Min: " << min << std::endl;
+    std::cout << "Max/Min: " << max/min << "\n";
+
     return 0;
-}
+}   
