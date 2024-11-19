229af1cd636bc325b0385f277e021a8ac6bf5247
bluzmija <172497559+bluzmija@users.noreply.github.com>
2024-11-19 09:02:28 +0100

    Changed insertion.cpp

--- Prog1/Ekstra/VjezbePTG/Pretrazivanje-sortiranje/insertion.cpp
+++ Prog1/Ekstra/VjezbePTG/Pretrazivanje-sortiranje/insertion.cpp
@@ -16,7 +16,7 @@ int main(){
     for (int i = 1; i < N; i++){
         int j = i - 1;
         int pom = A[i];
-        while ((j >= 0) && (pom < A[j])){
+        while ((j >= 0) && (A[j] > pom)){
             A[j+1] = A[j];
             j = j - 1;
         }
