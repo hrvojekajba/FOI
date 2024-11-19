f11c51f6c2b3f745a083c69ff85a38ada2c15649
bluzmija <172497559+bluzmija@users.noreply.github.com>
2024-11-19 08:59:48 +0100

    Added insertion.cpp

---
+++ Prog1/Ekstra/VjezbePTG/Pretrazivanje-sortiranje/insertion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(){
+    int N, A[100];
+
+    do{
+        std::cout << "N = ";
+        std::cin >> N;
+    }while(N > 100);
+
+    for (int i = 0; i < N; i++){
+        std::cout << "A[" << i << "]: ";
+        std::cin >> A[i];
+    }
+
+    for (int i = 1; i < N; i++){
+        int j = i - 1;
+        int pom = A[i];
+        while ((j >= 0) && (pom < A[j])){
+            A[j+1] = A[j];
+            j = j - 1;
+        }
+        A[j+1] = pom;
+    }
+
+    for (int i = 0; i < N; i++){
+        std::cout << A[i] << " ";
+    }
+    
+    std::cout << "\n";
+    return 0;
+}
