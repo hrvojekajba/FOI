812345bc0b64a30f78d7cd21960ee940352d1e57
Hrvoje <172497559+bluzmija@users.noreply.github.com>
2025-01-27 16:27:34 +0100

    Updated sedmi.cpp

--- Prog1/Ekstra/VjezbePTG/Zavrsna-PTG/sedmi.cpp
+++ Prog1/Ekstra/VjezbePTG/Zavrsna-PTG/sedmi.cpp
@@ -8,7 +8,7 @@ int main(){
 
     for (int i = 65; i < 91; i++){
         int broj_slova = 0;
-        for (int j = 0; j < 101; j++){
+        for (int j = 0; j < 100; j++){
             if (polje[j] == char(i) || polje[j] == char(i+32)) broj_slova++;
         }
         if (broj_slova == 0){
