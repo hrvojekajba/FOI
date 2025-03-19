1b24975347491b039be0c80f1d709465d035e5cd
Hrvoje <172497559+bluzmija@users.noreply.github.com>
2025-03-19 07:38:13 +0100

    Updated vjezba1.cpp

--- Prog2/Vjezba1/vjezba1.cpp
+++ Prog2/Vjezba1/vjezba1.cpp
@@ -1,74 +1,89 @@
-#include <cstring>
 #include <iostream>
+#include <string>
 #include <cmath>
-#include <iomanip>
-#include <cstring>
 #include "biblioteka_vrijeme.cc"
+using namespace std;
 
 void prvi(){
-    double suma = 0;
-    double *polje = new double [4] {3.56, 7.17, 5.12, 4.21};
-    for (int i = 0; i < 4; i++){
-        std::cout << "  polje[" << i << "]: " << polje[i] << "\n";
-        suma += polje[i];
-    }
-    std::cout << "Suma: " << suma << "\n";
+	cout << "\n---1. ZADATAK---\n";
+	string *polje = new string[3] {"Sijecanj", "Veljaca", "Ozujak"};
+	for (int i = 0; i < 3; i++){
+		cout << "polje[" << i << "]: " << polje[i] << "\n";
+	}
+	cout << "---KRAJ---\n\n";
 }
 
-double drugi (int a, int b){
-    a = abs(a);
-    b = abs(b);
-    return (double(a)*double(b))/2;
+float povrsina_kruga(float r){
+	abs(r);
+	return r*r*3.14;
+}
+
+void drugi(){
+	float r;
+	cout << "\n---2. ZADATAK---\n";
+	cout << "Unesite radijus kruga: "; cin >> r;
+	cout << "Povrsina kruga je: " << povrsina_kruga(r) << "\n";
+	cout << "---KRAJ---\n\n";
 }
 
 void treci(){
-    struct struktura {
-        int god;
-        char ime[50];
-    };
-
-    struktura var = {20, "Hrvoje"};
-    struktura copyvar;
-    memcpy(&copyvar, &var, sizeof(var));
-
-    std::cout << copyvar.god << " " << copyvar.ime << "\n";
+	cout << "\n---3. ZADATAK---\n";
+	
+	struct struktura{
+		int sifra;
+		char naziv[50];
+		int godina;
+	};
+	struktura *var1 = new struktura;
+	struktura *copyvar = new struktura;
+	
+	char ime_hladnjaka[50] = "Hladnjak21";
+	strcpy(var1->naziv, ime_hladnjaka);
+	
+	var1->sifra = 1582;
+	var1->godina = 2018;
+	
+	memcpy(copyvar, var1, sizeof(struktura));
+	
+	cout << "Kopirani podatci:\n" << "Sifra: " << copyvar->sifra << "\nNaziv: " << copyvar->naziv << "\nGodina proizvodnje: " << copyvar->godina;
+	cout << "\n---KRAJ---\n\n";
 }
 
 void cetvrti(){
-    int a,b,c;
-    do{
-        vrijeme_pocetak();
-        std::cout << "A: "; std::cin >> a;
-        std::cout << "B: "; std::cin >> b;
-        std::cout << "C: "; std::cin >> c;
-        vrijeme_kraj();
-        std::cout << "Vrijeme unosa stranica: " << vrijeme_proteklo() / 100 << "\n";
-    }while(a <= 0 || b <= 0 || c <= 0 || (a + b) <= c || (b + c) <= a || (c + a) <= b);
-    float s = (a + b + c) / 2.0;
-    std::cout << "S: " << s << "\n";
-    float povrsina = sqrt(s*(s-a)*(s-b)*(s-c));
-    std::cout << fixed << std::setprecision(2) << "Povrsina: " << povrsina << "\n";
+	cout << "\n---4. ZADATAK---\n";
+	int a,b,c;
+	do {
+		vrijeme_pocetak();
+		cout << "Unesite A: "; cin >> a;
+		cout << "Unesite B: "; cin >> b;
+		cout << "Unesite C: "; cin >> c;
+		vrijeme_kraj();
+		cout << "Vrijeme potrebno za unos stranica: " << vrijeme_proteklo() << "\n";
+	}while ((a <= 0) || (b <= 0) || (c <= 0) || (a > b+c) || (b > a+c) || (c > a+b));
+	
+	float s =float(a+b+c)/2;
+	float P = sqrt(s*(s-a)*(s-b)*(s-c));
+	cout << "Povrsina trokuta je: " << P << "\n";
+	
+	cout << "\n---KRAJ---\n\n";
 }
 
 int main(){
-    int izbor = 0;
-    do{
-        std::cout << "Izbornik:\n";
-        std::cout << "  1. Polje\n  2. Funkcija\n  3. Struktura\n  4. Mjerenje vremena\n  9. Izlaz iz programa\n";
-        std::cout << "Unesite izbor: ";
-        std::cin >> izbor;
-        switch (izbor){
-            case 1: prvi(); break;
-            case 2:
-                int a;
-                std::cout << "A: "; std::cin >> a;
-                int b;
-                std::cout << "B: "; std::cin >> b;
-                std::cout << "Povrsina trokuta: " << drugi(a,b) << "\n";
-                break;
-            case 3: treci(); break;
-            case 4: cetvrti(); break;
-        }
-    }while (izbor != 9);
-    return 0;
+	int izbor = 0;
+	
+	do{
+		cout << "Izbornik:\n"; 
+		cout << "  1. Polje\n" << "  2. Funkcija\n" << "  3. Struktura\n" << "  4. Mjerenje vremena\n" << "  9. Izlaz iz programa\n" << "Izbor: ";
+		cin >> izbor;
+		
+		switch (izbor) {
+			case 1: prvi(); break;
+			case 2: drugi(); break;
+			case 3: treci(); break;
+			case 4: cetvrti(); break;
+		}
+	}while(izbor != 9);
+	
+	system("PAUSE");
+	return 0;
 }
