835a1fa9fe007c2e7835ee9dec03d123db0adbf2
bluzmija <172497559+bluzmija@users.noreply.github.com>
2025-04-29 12:37:16 +0200

    Upadated vjezba6-1.cpp

--- Prog2/Vjezba6/vjezba6-1.cpp
+++ Prog2/Vjezba6/vjezba6-1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include "../biblioteka_vrijeme_i_binarno_pretrazivanje_linux.cc"
 using namespace std;
 
 struct tpredmet{
@@ -8,11 +9,6 @@ struct tpredmet{
     char naziv[50];
     char smjer[50];
     int broj_prijava;
-};
-
-struct tindeks{
-    int sifra;
-    int adresa;
 };
 
 fstream dat, ind;
@@ -38,10 +34,12 @@ void rucni_unos(char maticna[], char indeksna[]){
         dat.seekg(0, ios::end); //pokazivac citanja se postavlja na kraj datoteke
         brojac_zapisa = dat.tellg() / sizeof(tpredmet); //broj zapisa je adresa na kojoj je pokazivac citanja / velicinu predmeta
         dat.close(); dat.clear(); 
-        dat.open(maticna, ios::out | ios::in | ios::ate | ios::binary);
+        //otvara datoteke u potrebnim modovima
+        dat.open(maticna, ios::out | ios::in | ios::ate | ios::binary); 
         ind.open(indeksna, ios::out | ios::in | ios::ate | ios::binary);
     }
     
+    //unos informacija u maticnu datoteku
     brojac_zapisa++;
     cout << "\nZapis broja: " << brojac_zapisa << endl;
     element.rb_zapisa = brojac_zapisa;
@@ -50,10 +48,41 @@ void rucni_unos(char maticna[], char indeksna[]){
     cout << "Smjer: "; cin.getline(element.smjer, 50);
     cout << "Broj prijava: "; cin >> element.broj_prijava;
 
+    //unos informacija u indeksnu datoteku
     indeks.sifra = element.sifra;
     indeks.adresa = (int)dat.tellp(); //adresa je int vrijednost adrese na koju pokazuje pokazivac za pisanje
     dat.write((char*)&element, sizeof(tpredmet));
     ind.write((char*)&indeks, sizeof(tindeks));
+    dat.close(); dat.clear();
+    ind.close(); ind.clear();
+}
+
+void ispis(char maticna[], char indeksna[]){ //funkcija za ispis pomocu indeksne datoteke
+    int broj_zapisa = 0;
+    tpredmet element;
+    tindeks indeks;
+    dat.open(maticna, ios::in | ios::binary);
+    ind.open(indeksna, ios::in | ios::binary);
+
+    while(true){
+        ind.read((char*)&indeks, sizeof(tindeks)); //procita indeksnu datoteku
+        if(ind.eof()) break; //ako je kraj - break loop
+        else{
+            cout << "\n---Element---\n";
+            cout << "Sifra u indeksnoj datoteci: " << indeks.sifra << endl;
+            cout << "Adresa u indeksnoj datoteci: " << indeks.adresa << endl;
+            dat.seekg(indeks.adresa); //postavlja pokazivac za citanje na adresu koja je zapisana u indeksnoj
+            //ispisuje ostale podatke iz maticne datoteke
+            dat.read((char*)&element, sizeof(tpredmet));
+            cout << "Redni broj zapisa: " << element.rb_zapisa << endl;
+            cout << "Naziv predmeta: " << element.naziv << endl;
+            cout << "Smjer predmeta: " << element.smjer << endl;
+            cout << "Broj prijavljenih: " << element.broj_prijava << endl;
+            cout << "-------------\n";
+            broj_zapisa++;
+        }
+    }
+    cout << "\nBroj zapisa u datoteci: " << broj_zapisa << endl << endl;
     dat.close(); dat.clear();
     ind.close(); ind.clear();
 }
@@ -80,12 +109,25 @@ int main(){
                     cin.ignore();
                     cout << "Unesite ime maticne datoteke: "; cin.getline(maticna, 50);
                     cout << "Unesite ime indeksne datoteke: "; cin.getline(indeksna, 50);
+                    cout << endl;
                     kreiranje(maticna, indeksna);
                 }else{
                     kreiranje(maticna, indeksna);
+                    cout << endl;
                 }
+                break;
             case 2:
                 rucni_unos(maticna, indeksna);
+                break;
+            case 3:
+                break;
+            case 4:
+                ispis(maticna, indeksna);
+                break;
+            case 9:
+                break;
+            default:
+                cout << "\nPogresan unos\n";
         }
     }while(izbor != 9);
 }
