a2e2df49d4f2aee1dcd6a703b64a3742eb3342a8
hrvojekajba <172497559+hrvojekajba@users.noreply.github.com>
2025-10-21 20:46:26 +0200

    Changed vjezba1.c

--- OS/Vjezba-1/vjezba1.c
+++ OS/Vjezba-1/vjezba1.c
@@ -13,34 +13,16 @@ void vrijeme_ulaska() {
   printf("\n\nPrekidna rutina pozvana u %s\n", ctime(&vrijeme));
 }
 
-int odredi_razinu_prekida() {
-  int i;
-  printf("Razina prekida (0-8): ");
-  scanf("%d", &i);
-  return i;
-}
-
-void obrada_prekida() {
-  for (int i = 7; i > 0; i--) {
-    if (prioritet_prekida[i] == 1) {
-      for (int j = 5; j > 0; j--) {
-        printf("Rutina prekida razine %d: %d/5\n", i, j);
-        sleep(1);
-      }
-      prioritet_prekida[i] = 0;
-    }
-  }
-}
-
 void prekidna_rutina(int sig) {
   vrijeme_ulaska();
-  int i = odredi_razinu_prekida();
+
+  int i;
+  printf("\nRazina prekida: ");
+  scanf("%d", &i);
   prioritet_prekida[i] = 1;
-  obrada_prekida();
 }
 
 int main() {
-  // koristen sigaction jer sigset dela funny -- fml
   struct sigaction sigac = {};
   sigac.sa_handler = prekidna_rutina;
   sigaction(SIGINT, &sigac, NULL);
