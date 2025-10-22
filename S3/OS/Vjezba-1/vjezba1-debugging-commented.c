9d2ca94200ee1fd0cb5136cb763d641847dde519
hrvojekajba <172497559+hrvojekajba@users.noreply.github.com>
2025-10-22 12:37:50 +0200

    Added .clang-format and changed tab sizing on all files in S3/OS/Vjezba1

--- S3/OS/Vjezba-1/vjezba1-debugging-commented.c
+++ S3/OS/Vjezba-1/vjezba1-debugging-commented.c
@@ -10,85 +10,85 @@ int PRIORITET[N];
 int TEKUCI_PRIORITET = 0;
 
 void vrijeme_ulaska() {
-  time_t vrijeme;
-  time(&vrijeme);
-  printf("\nPrekidna rutina pozvana u %s\n", ctime(&vrijeme));
+    time_t vrijeme;
+    time(&vrijeme);
+    printf("\nPrekidna rutina pozvana u %s\n", ctime(&vrijeme));
 }
 
 void obrada_prekida(int n) {
-  printf("Obradujem prekid razine %d\n", n);
-  for (int i = 5; i > 0; i--) {
-    printf("Prekid razine %d: %d/5\n", n, i);
-    sleep(1);
-  }
-  printf("Zavrsio sa obradom\n\n");
+    printf("Obradujem prekid razine %d\n", n);
+    for (int i = 5; i > 0; i--) {
+        printf("Prekid razine %d: %d/5\n", n, i);
+        sleep(1);
+    }
+    printf("Zavrsio sa obradom\n\n");
 }
 
 void prekidna_rutina(int sig) {
-  vrijeme_ulaska();
+    vrijeme_ulaska();
 
-  int i;
-  printf("Razina prekida: ");
-  scanf("%d", &i);
-  printf("\n");
-  OZNAKA_CEKANJA[i] = 1;
+    int i;
+    printf("Razina prekida: ");
+    scanf("%d", &i);
+    printf("\n");
+    OZNAKA_CEKANJA[i] = 1;
 
-  int x = 1;
+    int x = 1;
 
-  do {
-    x = 0;
+    do {
+        x = 0;
 
-    // odreduje prekid najveceg prioriteta koji ceka obradu
-    for (int j = TEKUCI_PRIORITET; j < N; j++) {
-      if (OZNAKA_CEKANJA[j] != 0)
-        x = j;
-    }
+        // odreduje prekid najveceg prioriteta koji ceka obradu
+        for (int j = TEKUCI_PRIORITET; j < N; j++) {
+            if (OZNAKA_CEKANJA[j] != 0)
+                x = j;
+        }
 
-    // ako postoji prekid koji ceka obradu idi u obradu
-    if (x > 0) {
-      printf("USAO U IF, X: %d\n", x);
-      // postavlja se da je proces u obradi, tj. da je obraden
-      OZNAKA_CEKANJA[x] = 0;
-      // u PRIORITET[x] se postavlja prethodni tekuci proces
-      PRIORITET[x] = TEKUCI_PRIORITET;
-      printf("PRIORITET[x]: %d\n", PRIORITET[x]);
-      // postavlja se da je x trenutni proces
-      TEKUCI_PRIORITET = x;
-      printf("TEKUCI_PRIORITET: %d\n", TEKUCI_PRIORITET);
+        // ako postoji prekid koji ceka obradu idi u obradu
+        if (x > 0) {
+            printf("USAO U IF, X: %d\n", x);
+            // postavlja se da je proces u obradi, tj. da je obraden
+            OZNAKA_CEKANJA[x] = 0;
+            // u PRIORITET[x] se postavlja prethodni tekuci proces
+            PRIORITET[x] = TEKUCI_PRIORITET;
+            printf("PRIORITET[x]: %d\n", PRIORITET[x]);
+            // postavlja se da je x trenutni proces
+            TEKUCI_PRIORITET = x;
+            printf("TEKUCI_PRIORITET: %d\n", TEKUCI_PRIORITET);
 
-      // dodaj sigint u new_list
-      sigset_t sig_list = {};
-      sigaddset(&sig_list, SIGINT);
-      // odobri koristenje SIGINT
-      sigprocmask(SIG_UNBLOCK, &sig_list, NULL);
+            // dodaj sigint u new_list
+            sigset_t sig_list = {};
+            sigaddset(&sig_list, SIGINT);
+            // odobri koristenje SIGINT
+            sigprocmask(SIG_UNBLOCK, &sig_list, NULL);
 
-      printf("Ulazim u obradu prekida\n");
-      obrada_prekida(x);
-      printf("Izlazim iz obrade prekida\n");
+            printf("Ulazim u obradu prekida\n");
+            obrada_prekida(x);
+            printf("Izlazim iz obrade prekida\n");
 
-      // zabrani koristenje SIGINT
-      sigprocmask(SIG_BLOCK, &sig_list, NULL);
-      printf("sigsuspend prosao\n");
+            // zabrani koristenje SIGINT
+            sigprocmask(SIG_BLOCK, &sig_list, NULL);
+            printf("sigsuspend prosao\n");
 
-      TEKUCI_PRIORITET = PRIORITET[x];
-      printf("novi TEKUCI_PRIORITET: %d\n", TEKUCI_PRIORITET);
-    }
-  } while (x > 0);
+            TEKUCI_PRIORITET = PRIORITET[x];
+            printf("novi TEKUCI_PRIORITET: %d\n", TEKUCI_PRIORITET);
+        }
+    } while (x > 0);
 }
 
 int main() {
-  struct sigaction sig = {};
-  sig.sa_handler = prekidna_rutina;
-  sigaction(SIGINT, &sig, NULL);
+    struct sigaction sig = {};
+    sig.sa_handler = prekidna_rutina;
+    sigaction(SIGINT, &sig, NULL);
 
-  printf("Poceo osnovni program\n");
+    printf("Poceo osnovni program\n");
 
-  for (int i = 10; i > 0; i--) {
-    printf("Glavni program: %d/10\n", i);
-    sleep(1);
-  }
+    for (int i = 10; i > 0; i--) {
+        printf("Glavni program: %d/10\n", i);
+        sleep(1);
+    }
 
-  printf("Zavrsio osnovni program\n");
+    printf("Zavrsio osnovni program\n");
 
-  return 0;
+    return 0;
 }
--- S3/OS/Vjezba-1/vjezba1.c
+++ S3/OS/Vjezba-1/vjezba1.c
@@ -10,89 +10,89 @@ int PRIORITETI[N];
 int TEKUCI_PRIORITET = 0;
 
 void vrijeme_prekida() {
-  time_t vrijeme;
-  time(&vrijeme);
-  printf("\n\nPrekidna rutina pozvana u: %s\n", ctime(&vrijeme));
+    time_t vrijeme;
+    time(&vrijeme);
+    printf("\n\nPrekidna rutina pozvana u: %s\n", ctime(&vrijeme));
 }
 
 void razina_prekida() {
-  int razina = 8;
+    int razina = 8;
 
-  do {
-    printf("Razina prekida: ");
-    scanf("%d", &razina);
-  } while (razina > 7 || razina < 0);
-  printf("\n");
+    do {
+        printf("Razina prekida: ");
+        scanf("%d", &razina);
+    } while (razina > 7 || razina < 0);
+    printf("\n");
 
-  OZNAKA_CEKANJA[razina] = 1;
+    OZNAKA_CEKANJA[razina] = 1;
 }
 
 void obrada_prekida(int n) {
-  printf("Obradujem prekid razine %d\n", n);
-  for (int i = 1; i <= 5; i++) {
-    printf("Prekid razine %d: %d/5\n", n, i);
-    sleep(1);
-  }
-  printf("Zavrsio s obradom prekida razine %d\n\n", n);
+    printf("Obradujem prekid razine %d\n", n);
+    for (int i = 1; i <= 5; i++) {
+        printf("Prekid razine %d: %d/5\n", n, i);
+        sleep(1);
+    }
+    printf("Zavrsio s obradom prekida razine %d\n\n", n);
 }
 
 void prekidna_rutina(int sig) {
-  // ispisi vrijeme pozivanja prekida
-  vrijeme_prekida();
+    // ispisi vrijeme pozivanja prekida
+    vrijeme_prekida();
 
-  // odredi razinu prekida
-  razina_prekida();
+    // odredi razinu prekida
+    razina_prekida();
 
-  // helper varijabla
-  int x = 1;
+    // helper varijabla
+    int x = 1;
 
-  do {
-    x = 0;
+    do {
+        x = 0;
 
-    // provjeri ako postoji proces veceg prioriteta
-    // koji je na cekanju
-    for (int j = TEKUCI_PRIORITET; j < N; j++) {
-      if (OZNAKA_CEKANJA[j] != 0)
-        x = j;
-    }
+        // provjeri ako postoji proces veceg prioriteta
+        // koji je na cekanju
+        for (int j = TEKUCI_PRIORITET; j < N; j++) {
+            if (OZNAKA_CEKANJA[j] != 0)
+                x = j;
+        }
 
-    if (x > 0) {
-      OZNAKA_CEKANJA[x] = 0;
-      PRIORITETI[x] = TEKUCI_PRIORITET;
-      TEKUCI_PRIORITET = x;
+        if (x > 0) {
+            OZNAKA_CEKANJA[x] = 0;
+            PRIORITETI[x] = TEKUCI_PRIORITET;
+            TEKUCI_PRIORITET = x;
 
-      // odredi listu signala koje
-      // treba blokirati / odblokirati
-      sigset_t siglist = {};
-      sigaddset(&siglist, SIGINT);
+            // odredi listu signala koje
+            // treba blokirati / odblokirati
+            sigset_t siglist = {};
+            sigaddset(&siglist, SIGINT);
 
-      // odblokiraj SIGINT
-      sigprocmask(SIG_UNBLOCK, &siglist, NULL);
+            // odblokiraj SIGINT
+            sigprocmask(SIG_UNBLOCK, &siglist, NULL);
 
-      // obradi prekid
-      obrada_prekida(x);
+            // obradi prekid
+            obrada_prekida(x);
 
-      // blokiraj SIGINT
-      sigprocmask(SIG_BLOCK, &siglist, NULL);
+            // blokiraj SIGINT
+            sigprocmask(SIG_BLOCK, &siglist, NULL);
 
-      TEKUCI_PRIORITET = PRIORITETI[x];
-    }
-  } while (x > 0);
+            TEKUCI_PRIORITET = PRIORITETI[x];
+        }
+    } while (x > 0);
 }
 
 int main() {
-  // definiraj koja funckcija se poziva sa ctrl+c
-  struct sigaction sigac = {};
-  sigac.sa_handler = prekidna_rutina;
-  sigaction(SIGINT, &sigac, NULL);
+    // definiraj koja funckcija se poziva sa ctrl+c
+    struct sigaction sigac = {};
+    sigac.sa_handler = prekidna_rutina;
+    sigaction(SIGINT, &sigac, NULL);
 
-  // glavna rutina
-  printf("\nObradujem glavnu rutinu:\n");
-  for (int i = 1; i <= 10; i++) {
-    printf("Glavna rutina: %d/10\n", i);
-    sleep(1);
-  }
-  printf("Zavrsio s obradom glavne rutine.\n");
+    // glavna rutina
+    printf("\nObradujem glavnu rutinu:\n");
+    for (int i = 1; i <= 10; i++) {
+        printf("Glavna rutina: %d/10\n", i);
+        sleep(1);
+    }
+    printf("Zavrsio s obradom glavne rutine.\n");
 
-  return 0;
+    return 0;
 }
